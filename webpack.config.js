d275048ead6a08ca8f54dade0dc9d22f65cdb0fa
kuohohoh <rhodonite22@gmail.com>
2025-02-03 20:27:32 +0900

    sdf

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '2048 Game Ver2',
+            title: '2048 Game',
         })
     ],
     devServer: {
